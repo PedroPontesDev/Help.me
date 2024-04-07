400b0d98997e6459ea22ce38afc895b9c4b5b56f
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2024-04-07 11:36:04 -0300

    Project Iniciated

--- Help.me/src/main/java/com/devPontes/Help/me/Application.java
+++ Help.me/src/main/java/com/devPontes/Help/me/Application.java
@@ -1,4 +1,4 @@
-package com.br.trentor.Help.me;
+package com.devPontes.Help.me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Help.me/src/test/java/com/devPontes/Help/me/ApplicationTests.java
+++ Help.me/src/test/java/com/devPontes/Help/me/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.br.trentor.Help.me;
+package com.devPontes.Help.me;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
