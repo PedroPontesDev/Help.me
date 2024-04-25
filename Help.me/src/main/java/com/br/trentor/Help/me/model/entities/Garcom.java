a2a434c8e2c08cbeb3005e66393318fff3c1e526
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2024-04-25 18:39:44 -0300

    Last Updates

--- Help.me/src/main/java/com/br/trentor/Help/me/model/entities/Garcom.java
+++ Help.me/src/main/java/com/br/trentor/Help/me/model/entities/Garcom.java
@@ -20,76 +20,74 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_garcom")
 public class Garcom extends Usuario {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(name = "horario_entrada_dia")
-    private LocalDateTime horarioEntrada;
+	@Column(name = "horas_trabalhadas_semana")
+	private LocalDateTime horasTrabalhadasSemana;
 
-    @Column(name = "horario_saida_dia")
-    private LocalDateTime horarioSaida;
+	@Column(name = "total_horas_trabalhadas_mes")
+	private Duration horasTrabalhadaMes;
 
-    @Column(name = "total_horas_trabalhadas_mes")
-    private Duration horasTrabalhadas;
-    
-    @Column(name = "salario_funcionario")
-    private BigDecimal salario;
+	@Column(name = "salario_funcionario")
+	private BigDecimal salario;
 
-    @OneToMany(mappedBy = "garcom")
-    private Set<Comanda> comandaDoGarcom = new TreeSet<>();
+	@OneToMany(mappedBy = "garcom")
+	private Set<Comanda> comandaDoGarcom = new TreeSet<>();
 
-    @OneToOne
-    @JoinColumn(name = "tb_permissao")
-    Role permissao;
+	@OneToOne
+	private Role permission;
 
-    public Garcom(Long id, String nome, String username, String password, String cpf, LocalDateTime horarioEntrada,
-            LocalDateTime horarioSaida, Duration horasTrabalhadas, BigDecimal salario, Set<Comanda> comandaDoGarcom,
-            Role permissao) {
-        super(id, nome, username, password, cpf, permissao);
-        this.horarioEntrada = horarioEntrada;
-        this.horarioSaida = horarioSaida;
-        this.horasTrabalhadas = horasTrabalhadas;
-        this.salario = salario;
-        this.comandaDoGarcom = comandaDoGarcom;
-        this.permissao = permissao;
-    }
+	public Garcom(Long id, String nome, String username, String password, String cpf, Role permissao,
+			LocalDateTime horasTrabalhadasSemana, Duration horasTrabalhadaMes, BigDecimal salario,
+			Set<Comanda> comandaDoGarcom, Role permission) {
+		super(id, nome, username, password, cpf, permission);
+		this.horasTrabalhadasSemana = horasTrabalhadasSemana;
+		this.horasTrabalhadaMes = horasTrabalhadaMes;
+		this.salario = salario;
+		this.comandaDoGarcom = comandaDoGarcom;
+		this.permission = permission;
+	}
 
-    public Garcom() {
-        super();
-    }
+	public Garcom() {
+		super();
+	}
 
-    // Getters e setters
+	// Getters e setters
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Garcom other = (Garcom) obj;
-        return Objects.equals(id, other.id);
-    }
-    
-    public Long calcularHorasTrabalhadas(LocalDateTime horarioEntrada, LocalDateTime horarioSaida) {
-    	Duration calculo = Duration.between(horarioEntrada, horarioSaida);
-    	Long horasTrabalhadas = calculo.toHours();
-    	return horasTrabalhadas;
-    }
-    
-    @Override
-    public String toString() {
-        return "Funcionario [id=" + id + ", horarioEntrada=" + horarioEntrada + ", horarioSaida=" + horarioSaida
-                + ", horasTrabalhadas=" + horasTrabalhadas + ", salario=" + salario + ", comandaDoGarcom="
-                + comandaDoGarcom + ", permissao=" + permissao + "]";
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Garcom other = (Garcom) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public Long calcularHorasTrabalhadas(LocalDateTime horarioEntrada, LocalDateTime horarioSaida) {
+		Duration calculo = Duration.between(horarioEntrada, horarioSaida);
+		Long horasTrabalhadas = calculo.toHours();
+		return horasTrabalhadas;
+	}
+
+	@Override
+	public String toString() {
+		return "Garcom [id=" + id + ", horasTrabalhadasSemana=" + horasTrabalhadasSemana + ", horasTrabalhadaMes="
+				+ horasTrabalhadaMes + ", salario=" + salario + ", comandaDoGarcom=" + comandaDoGarcom + ", permission="
+				+ permission + "]";
+	}
+
+	
+	
 }
